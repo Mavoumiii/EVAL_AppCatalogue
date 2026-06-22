cad8a631a51cf439b673020ad2a2c54381700abf
Mavoumiii <natcho.ab@hotmail.com>
2026-06-22 15:28:31 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/EVAL_AppFormation/'
+  base: '/EVAL_AppCatalogue/'
 })
